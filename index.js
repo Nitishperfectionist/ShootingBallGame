5da2312aaceaf77a7843ceacfafb55b1370a1a3d
Nitish kumar <nitishkumar2809@gmail.com>
2024-06-09 13:47:45 +0530

    completed till all css part except functionality

--- index.js
+++ index.js
@@ -27,11 +27,12 @@ class Player{
 }
 //weapon
 class Weapon{
-    constructor(x,y,radius,color){
+    constructor(x,y,radius,color,velocity){
        this.x=x;
        this.y=y;
        this.radius=radius;
        this.color=color;
+       this.velocity=velocity;
     
     }
     draw(){
@@ -40,6 +41,34 @@ class Weapon{
        context.fillStyle=this.color;
        
        context.fill();
+    }
+    update(){
+        this.draw();
+        this.x+=this.velocity.x;
+        this.y+=this.velocity.y;
+    }
+}
+//enemy
+class Enemy{
+    constructor(x,y,radius,color,velocity){
+       this.x=x;
+       this.y=y;
+       this.radius=radius;
+       this.color=color;
+       this.velocity=velocity;
+    
+    }
+    draw(){
+       context.beginPath();
+       context.arc(this.x, this.y, this.radius, Math.PI / 180 * 0, Math.PI / 180 * 360, false);
+       context.fillStyle=this.color;
+       
+       context.fill();
+    }
+    update(){
+        this.draw();
+        this.x+=this.velocity.x;
+        this.y+=this.velocity.y;
     }
 }
 //player
@@ -50,24 +79,70 @@ const newPlayer=new Player(
     `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`
 );
 const weapon=[]
+const enemies=[]
+
+const spawnEnemy=()=>{
+    const enemySize=Math.random()*(40-5)+5;
+    const enemyColor= `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`
+    let random;
+    if(Math.random()<0.5){
+     random={
+        x:Math.random()<0.5 ? canvas.width + enemySize : 0-enemySize,
+        y:Math.random()*canvas.height,
+     }
+    }else{
+        random={
+            x:Math.random() * canvas.width,
+            y:Math.random()<0.5 ? canvas.height + enemySize : 0-enemySize,
+           
+         }
+    }
+    const angle=Math.atan2(
+        canvas.height / 2 - random.y,
+        canvas.width / 2 - random.x
+    )
+    const velocity={
+        x:Math.cos(angle)*5,
+        y:Math.sin(angle)*5,
+       
+    }
+    enemies.push(new Enemy(random.x,random.y,enemySize,enemyColor,velocity))
+}
 function Animation(){
     requestAnimationFrame(Animation);
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    
     
 newPlayer.draw();
 
 weapon.forEach((el)=>{
-    el.draw();
+    el.update();
+})
+enemies.forEach((el)=>{
+    el.update();
 })
 }
+setInterval(spawnEnemy,1000)
+
 canvas.addEventListener("click",(ele)=>{
+    const angle=Math.atan2(
+        ele.clientY - canvas.height / 2,
+        ele.clientX - canvas.width / 2,
+    )
+    const velocity={
+        x:Math.cos(angle)*5,
+        y:Math.sin(angle)*5,
+       
+    }
     weapon.push(new Weapon(
-        ele.clientX,
-        ele.clientY,
+        canvas.width/2,
+        canvas.height/2,
         8,
-        `rgb(${Math.random()*250},${Math.random()*250},${Math.random()*250})`
+        "white",
+        velocity
     ));
 })
 Animation();
 
-//bullet
+
 
